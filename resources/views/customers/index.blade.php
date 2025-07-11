df047813a1e8c95b51fea271ef3252234c95a1bb
ujvarivera <ujvariveronika0320@gmail.com>
2025-07-11 21:06:16 +0200

    css added to list page

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -2,48 +2,55 @@
 
 @section('content')
 
-<h1>{{ __('Ügyfelek')}}</h1>
+<div class="card py-4 px-4 mx-4 rounded-4">
+    <h1>{{ __('Ügyfelek')}}</h1>
 
-@if(session('success'))
-    <div class="alert alert-success">
-        {{ session('success') }}
+    @if(session('success'))
+        <div class="alert alert-success">
+            {{ session('success') }}
+        </div>
+    @endif
+
+    <div class="card m-4 rounded-4">
+        <div class="card-body p-4">
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col" class="text-secondary">{{ __('Név')}}</th>
+                        <th scope="col" class="text-secondary">{{ __('Email') }}</th>
+                        <th scope="col" class="text-secondary">{{ __('Telefonszám')}}</th>
+                        <th scope="col" class="text-secondary">{{ __('Művelet')}}</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($customers as $customer)
+                    <tr class="align-middle">
+                        <td>
+                            <img src="{{ asset('images/avatars/avatar_0' . $customer->avatar . '.png') }}" alt="Avatar" width="50" height="50" class="rounded-circle">
+                            <span class="ms-3 font-weight-bold">{{ $customer->name }}</span>
+                        </td>
+                        <td>{{ $customer->email }}</td>
+                        <td>{{ $customer->phone }}</td>
+                        <td>
+                            <form action="{{ route('customers.destroy', $customer->id) }}" method="POST" onsubmit="return confirm('Are you sure you want to delete this customer?');">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-link p-0 m-0 text-black text-uppercase font-weight-bold" style="text-decoration: none;">
+                                    {{ __('Törlés') }}
+                                </button>
+                            </form>
+                        </td>
+                        </tr>
+                        @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
-@endif
 
-<table class="table table-hover">
-  <thead>
-    <tr>
-      <th scope="col">{{ __('Név')}}</th>
-      <th scope="col">{{ __('Email') }}</th>
-      <th scope="col">{{ __('Telefonszám')}}</th>
-      <th scope="col">{{ __('Művelet')}}</th>
-    </tr>
-  </thead>
-  <tbody>
-      @foreach ($customers as $customer)
-      <tr>
-          <td>
-            <img src="{{ asset('images/avatars/avatar_0' . $customer->avatar . '.png') }}" alt="Avatar" width="50" height="50">
-            {{ $customer->name }}
-          </td>
-          <td>{{ $customer->email }}</td>
-          <td>{{ $customer->phone }}</td>
-          <td>
-            <form action="{{ route('customers.destroy', $customer->id) }}" method="POST" onsubmit="return confirm('Are you sure you want to delete this customer?');">
-                @csrf
-                @method('DELETE')
-                <button type="submit" class="btn btn-link p-0 m-0 text-black" style="text-decoration: none;">
-                    {{ __('Törlés') }}
-                </button>
-            </form>
-          </td>
-        </tr>
-        @endforeach
-  </tbody>
-</table>
-
-<a href="{{ route('customers.create') }}" class="btn btn-primary mb-3">
-    {{ __('ÚJ ÜGYFÉL HOZZÁADÁSA') }}
-</a>
-
+    <div class="text-center text-small">
+        <a href="{{ route('customers.create') }}" class="btn btn-primary mb-3 text-uppercase px-5">
+            <small>{{ __('Új ügyfél hozzáadása') }}</small>
+        </a>
+    </div>
+</div>
 @endsection
