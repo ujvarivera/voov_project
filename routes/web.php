4f6c08b938d144a0986005b5d8f90f7ddcf0cdac
ujvarivera <ujvariveronika0320@gmail.com>
2025-07-11 22:49:16 +0200

    env.example + readme added

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use App\Http\Controllers\CustomerController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return redirect()->route('customers.index');
+});
+
 Route::resource('customers', CustomerController::class);
 
